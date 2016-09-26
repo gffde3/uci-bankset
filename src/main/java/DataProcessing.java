0db9d47145ab13a78e83c4bc9f583c39f7ab4809
geoffdrre <geoff.j.dwyer@gmail.com>
2016-09-26 21:27:36 +1000

    new

---
+++ src/main/java/DataProcessing.java
@@ -0,0 +1,51 @@
+import org.apache.spark.SparkConf;
+import org.apache.spark.SparkContext;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.datavec.api.records.reader.RecordReader;
+import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
+import org.datavec.api.transform.TransformProcess;
+import org.datavec.api.transform.condition.ConditionOp;
+import org.datavec.api.transform.condition.column.CategoricalColumnCondition;
+import org.datavec.api.transform.condition.column.DoubleColumnCondition;
+import org.datavec.api.transform.filter.ConditionFilter;
+import org.datavec.api.transform.schema.Schema;
+import org.datavec.api.transform.transform.condition.ConditionalReplaceValueTransform;
+import org.datavec.api.transform.transform.time.DeriveColumnsFromTimeTransform;
+import org.datavec.api.transform.transform.time.StringToTimeTransform;
+import org.datavec.api.util.ClassPathResource;
+import org.datavec.api.writable.DoubleWritable;
+import org.datavec.api.writable.Writable;
+import org.datavec.spark.functions.RecordReaderFunction;
+import org.datavec.spark.transform.SparkTransformExecutor;
+import org.datavec.spark.transform.misc.StringToWritablesFunction;
+import org.datavec.spark.transform.misc.WritablesToStringFunction;
+import org.joda.time.DateTimeFieldType;
+import org.joda.time.DateTimeZone;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+public class DataProcessing {
+
+    Schema inputDataSchema = new Schema.Builder()
+            .addColumnInteger("age")
+            .addColumnCategorical("job", Arrays.asList("admin.","blue-collar","entrepreneur","housemaid",
+                    "management","retired","self-employed","services","student","technician","unemployed","unknown"))
+
+
+            //We can define a single column
+            .addColumnString("DateTimeString")
+            //Or for convenience define multiple columns of the same type
+            .addColumnsString("CustomerID", "MerchantID")
+            //We can define different column types for different types of data:
+            .addColumnInteger("NumItemsInTransaction")
+            .addColumnCategorical("MerchantCountryCode", Arrays.asList("USA","CAN","FR","MX"))
+            //Some columns have restrictions on the allowable values, that we consider valid:
+            .addColumnDouble("TransactionAmountUSD",0.0,null,false,false)   //$0.0 or more, no maximum limit, no NaN and no Infinite values
+            .addColumnCategorical("FraudLabel", Arrays.asList("Fraud","Legit"))
+            .build();
+
+
+}
